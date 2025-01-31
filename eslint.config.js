0de845d9f4b611d5a14af51c961eef2fe4bf8753
evidencia <evidenciadavid2016@gmail.com>
2025-01-31 22:56:22 +0000

    teste

--- eslint.config.js
+++ eslint.config.js
@@ -7,15 +7,12 @@ export default [
     plugins: {
       import: eslintPluginImport
     },
-    overrides: [
-      {
-        files: ['*.ts', '*.tsx'], // Aplica as regras apenas para arquivos TypeScript
-        rules: {
-          'import/no-unresolved': 'error',
-          'import/order': 'warn',
-          'react/react-in-jsx-scope': 'off'
-        }
-      }
-    ]
+
+    files: ['*.ts', '*.tsx'], // Aplica as regras apenas para arquivos TypeScript
+    rules: {
+      'import/no-unresolved': 'error',
+      'import/order': 'warn',
+      'react/react-in-jsx-scope': 'off'
+    }
   }
 ]
