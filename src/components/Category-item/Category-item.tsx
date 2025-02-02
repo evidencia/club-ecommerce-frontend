43b7f23de507e0ecbd1f5774c16936f1747c1a88
evidencia <evidenciadavid2016@gmail.com>
2025-02-02 00:31:49 +0000

    feat: add category item component

---
+++ src/components/Category-item/Category-item.tsx
@@ -0,0 +1,23 @@
+import Category from "../../types/categories.types";
+
+import './Category.styles.css'
+
+interface CategoryItemProps {
+  category: Category
+}
+
+function CategoryItem({ category }: CategoryItemProps) {
+  return (
+    <div 
+      className="category-item-container"
+      style={{backgroundImage: category.imageUrl}}
+    >
+      <div className="category-name-container">
+        <p>{category.displayName}</p>
+        <p>Explorar</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
