adefaaf2ad4f0284f8bcb359ec52d7f94f9e59ca
evidencia <evidenciadavid2016@gmail.com>
2025-02-03 00:41:05 +0000

    feat: add new route login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Home"
+import LoginPage from "./pages/login/LoginPage"
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/about' element={<Home />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
   )
 }
