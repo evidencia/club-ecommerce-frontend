5f93be35d056d980c76d9cccb358a752d233fecb
evidencia <evidenciadavid2016@gmail.com>
2025-02-05 15:04:57 +0000

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDe6TB6ca-dguqUJ6FEjCPRGKgN75fOb7w',
+  authDomain: 'club-ecommerce-967d3.firebaseapp.com',
+  projectId: 'club-ecommerce-967d3',
+  storageBucket: 'club-ecommerce-967d3.firebasestorage.app',
+  messagingSenderId: '115631025835',
+  appId: '1:115631025835:web:335449845f8f49d13b91ee'
+}
+
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
