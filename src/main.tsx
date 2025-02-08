cb4077d628b2df6060d7090f32494c0402a37c92
evidencia <evidenciadavid2016@gmail.com>
2025-02-08 23:38:19 +0000

    feat: add UserContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.tsx'
 import './main.styles.css'
+import UserContextProvider from './contexts/user.context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
