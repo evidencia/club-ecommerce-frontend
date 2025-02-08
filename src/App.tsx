9c5519d54d63b50c96cb2a69098da2d203e95ac5
evidencia <evidenciadavid2016@gmail.com>
2025-02-08 23:41:33 +0000

    feat: ensure user context is updated on onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,29 @@ import Home from "./pages/home/Home"
 import LoginPage from "./pages/login/LoginPage"
 import SignUp from "./pages/Sign-up/Sign-up.page"
 import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "./config/firebase.config"
+import { auth, db } from "./config/firebase.config"
+import { UserContext } from "./contexts/user.context"
+import { useContext } from "react"
+import { collection, getDocs, query, where } from "firebase/firestore"
 
 function App() {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user)
+  const {isAuthenticated, loginUser, logoutUser} = useContext(UserContext)
+
+  onAuthStateChanged(auth, async(user) => {
+    const isSigningOut = isAuthenticated && !user
+    if (isSigningOut) {
+      return logoutUser()
+    }
+
+    const isSigningIn = !isAuthenticated && user
+    if (isSigningIn) {
+      const querySnapshot = await getDocs(
+        query(collection(db, 'users'), where('id', '==', user.uid))
+      )
+
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+      return loginUser(userFromFirestore as any)
+    }
   })
   
   return (
