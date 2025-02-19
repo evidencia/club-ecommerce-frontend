39c27f9ca03b2cc221e4f870c36c4056cc170f18
evidencia <evidenciadavid2016@gmail.com>
2025-02-19 16:10:40 +0000

    feat: add redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './main.styles.css'
 import UserContextProvider from './contexts/user.context.tsx'
 import CategoryContextProvider from './contexts/category.context.tsx'
 import CartContextProvider from './contexts/cart.context.tsx'
 import Cart from './components/cart/cart.component.tsx'
+import store from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <UserContextProvider>
         <CategoryContextProvider>
           <CartContextProvider>
@@ -19,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
           </CartContextProvider>
         </CategoryContextProvider>
       </UserContextProvider>
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
